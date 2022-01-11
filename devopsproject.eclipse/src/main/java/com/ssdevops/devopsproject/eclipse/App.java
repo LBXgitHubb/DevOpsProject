48785967c0c551b819c682500a4218bf02c0284c
LBXgitHubb <bing6448@gmail.com>
2022-01-11 17:16:02 +0800

    Initial Commit

---
+++ devopsproject.eclipse/src/main/java/com/ssdevops/devopsproject/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.ssdevops.devopsproject.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
